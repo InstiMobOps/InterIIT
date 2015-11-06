7e236e4ae4e35a0e17d5b378c5a66d41f853264e
arun <arunpadyan@gmail.com>
2015-11-06 11:52:02 +0530

    some changes

--- app/src/main/java/org/interiitsports/interiit/MainActivity.java
+++ app/src/main/java/org/interiitsports/interiit/MainActivity.java
@@ -1,5 +1,6 @@
 package org.interiitsports.interiit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,7 +83,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.map) {
+            Intent newI = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(newI);
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
