ae8f165e5a78685b4bafd2714d64680387692a2e
arun <arunpadyan@gmail.com>
2015-11-06 15:30:20 +0530

    some changes 2

--- app/src/main/java/org/interiitsports/interiit/MainActivity.java
+++ app/src/main/java/org/interiitsports/interiit/MainActivity.java
@@ -1,10 +1,7 @@
 package org.interiitsports.interiit;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,7 +11,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.Map;
+import org.interiitsports.interiit.Fragments.GalleryFragment;
+import org.interiitsports.interiit.Fragments.MapFragment;
+import org.interiitsports.interiit.Fragments.NotificationFragment;
+import org.interiitsports.interiit.Fragments.ScheduleFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -26,14 +26,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -43,6 +35,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new NotificationFragment());
+        fragmentTransaction.commit();
+
     }
 
     @Override
@@ -82,25 +80,37 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.map) {
-            Intent newI = new Intent(MainActivity.this, MapsActivity.class);
-            startActivity(newI);
-            // Handle the camera action
+        showViews();
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        if (id == R.id.nav_notification) {
+            fragmentTransaction.replace(R.id.fragment_container, new NotificationFragment());
+        } else if (id == R.id.nav_map) {
+            hideViews();
+            fragmentTransaction.replace(R.id.fragment_container, new MapFragment());
+        } else if (id == R.id.nav_schedule) {
+            fragmentTransaction.replace(R.id.fragment_container, new ScheduleFragment());
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+            fragmentTransaction.replace(R.id.fragment_container, new GalleryFragment());
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.web_site) {
 
         }
+        fragmentTransaction.commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    public void hideViews() {
+        getSupportActionBar().hide();
+
+    }
+
+    public void showViews() {
+        getSupportActionBar().show();
+    }
+
+
 }
--- app/src/main/java/org/interiitsports/interiit/SplashScreen.java
+++ app/src/main/java/org/interiitsports/interiit/SplashScreen.java
@@ -1,6 +1,8 @@
 package org.interiitsports.interiit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,17 +15,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        new CountDownTimer(3000, 1000) {
+            public void onFinish() {
+                Intent startActivity = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(startActivity);
+                finish();
             }
-        });
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+        }.start();
+
     }
 
 }
